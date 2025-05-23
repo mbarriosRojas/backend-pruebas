d2726b82866019e9159f3c170b51bb7813ed6322
mbarriosRojas <110307709+mbarriosRojas@users.noreply.github.com>
2025-05-23 13:32:30 +0000

    add example api

--- routes/api/pedido.js
+++ routes/api/pedido.js
@@ -1,5 +1,7 @@
 const Pedido = require("../../controllers/pedido_controller");
 
 module.exports = function (router) {
-
+router.get("/test-conexion", (req, res) => {
+    res.status(200).send("Conexión exitosa al servidor!");
+  });
 };
